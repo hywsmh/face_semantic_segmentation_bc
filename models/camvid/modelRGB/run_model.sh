5d2e6625fde2e259deac3370353926211c7301b7
yixi1992 <yixi1992@gmail.com>
2016-04-16 02:58:10 -0400

    Copy weights then surgery deconv layers; Fixed Deconv layers to lr_mult: 0; iter_size=1->1000

--- models/camvid/modelRGB/run_model.sh
+++ models/camvid/modelRGB/run_model.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-#SBATCH -t 8:00:00
+#SBATCH --time 4:00:00
 #SBATCH -N 1
 #SBATCH --gres=gpu
+#SBATCH --partition=gpu
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=6
 
-source /lustre/yixi/decouplednet/DecoupledNet/inference/load_deeplab_dependencies.sh
-source ../../load_deeplab_dependencies.sh
+#source ./load_caffe_dependencies.sh
 
 python ./solve.py > solve.out
