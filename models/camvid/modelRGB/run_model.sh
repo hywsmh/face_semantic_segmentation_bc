7dedcdd138d62eb0a04189ad617111a994f91454
yixi1992 <yixi1992@gmail.com>
2016-04-18 04:02:50 -0400

    snapshots_camvid500500/train_lr1e-10_12000_1e-9_8000_9000_1e-10/ where the starting Test_loss=63k

--- models/camvid/modelRGB/run_model.sh
+++ models/camvid/modelRGB/run_model.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-#SBATCH --time 4:00:00
+#SBATCH --time 15:00:00
 #SBATCH -N 1
 #SBATCH --gres=gpu
 #SBATCH --partition=gpu
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=6
 
-#source ./load_caffe_dependencies.sh
 
-python ./solve.py > solve.out
+python ./solve.py
